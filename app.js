72e598687024e7c55c00bd7a78c6c5cd31995cce
Allyson Camp <Allysoncamp92@gmail.com>
2023-01-11 15:18:26 -0800

    fixed counter and added photos with template literal

--- app.js
+++ app.js
@@ -16,34 +16,41 @@ let middleCount = 0;
 let bottomCount = 0;
 // set state for all of the character's catchphrases
 
+
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-    headEl.textContent = headCount;
+   
+    const headValue = headDropdown.value;
+    
     // increment the head change count state
     headCount++;
+   
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-    
+    headEl.style.backgroundImage = `url(./assets/${headValue}-head.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+
 });
 
 middleDropdown.addEventListener('change', () => {
      // get the value of the middle dropdown
-     middleEl.textContent = headCount;
+    const middleValue = middleDropdown.value;
      // increment the middle change count state
-     middleCount++;
+    middleCount++;
+   
      // update the dom for the middle (use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    middleEl.style.backgroundImage = `url(./assets/${middleValue}-middle.png)`;
      // update the stats to show the new count (call displayStats() to do this work)
 });
 
 
 bottomDropdown.addEventListener('change', () => {
       // get the value of the bottom dropdown
-      bottomEl.textContent = headCount;
+    const bottomValue = bottomDropdown.value;
       // increment the bottom change count state
-      bottomCount++;
+    bottomCount++;
+ 
       // update the dom for the bottom (use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-      
+    bottomEl.style.backgroundImage = `url(./assets/${bottomValue}-pants.png)`;
       // update the stats to show the new count (call displayStats() to do this work)
 });
 
